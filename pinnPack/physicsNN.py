d5a6da03cdb828ae22729db721bc4f3e1a0c5d8e
NdokiA <andikoputrapk@student.ub.ac.id>
2025-05-06 03:46:00 +0000

    Class for PINN purpose

---
+++ pinnPack/physicsNN.py
@@ -0,0 +1,310 @@
+from pinnPack import network, pinnUtils
+import numpy as np
+from tqdm import trange
+import torch
+import torch.nn.functional as F
+
+class phyModel():
+    def __init__(self, tx_init, uv_init, tx_col, tx_exp, uv_exp, txU_bound, txL_bound,
+                 layers, parameters, optimizer, net = 'DNN',
+                 act = 'tanh', encode_dim = None, causal_decay = False, curriculum = None, progressive = None,
+                 epsilon_weight = 1e-3,  lambda_bound = 1, lambda_init = 1, lambda_r = 1):
+        
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
+        #Model
+        self.net = net
+        self.layers = layers 
+        if self.net == 'DNN':
+            self.dnn = network.DNN(layers, act = act, encode_dim = encode_dim)
+        else: #Pretrained model path
+            self.dnn = network.DNN(layers, act = act, encode_dim = encode_dim)
+            state_dict = torch.load(self.net)
+            self.dnn.load_state_dict(state_dict)
+        self.dnn.to(self.device) 
+        
+        #Dataset Preparation 
+        self.t_init = torch.tensor(tx_init[0], requires_grad = True).float().to(self.device); 
+        self.x_init = torch.tensor(tx_init[1], requires_grad = True).float().to(self.device); 
+        
+        self.tU_bound =  torch.tensor(txU_bound[0], requires_grad = True).float().to(self.device); 
+        self.xU_bound =  torch.tensor(txU_bound[1], requires_grad = True).float().to(self.device); 
+        self.tL_bound =  torch.tensor(txL_bound[0], requires_grad = True).float().to(self.device); 
+        self.xL_bound =  torch.tensor(txL_bound[1], requires_grad = True).float().to(self.device); 
+        
+        self.t_col = torch.tensor(tx_col[0], requires_grad = True).float().to(self.device); 
+        self.x_col = torch.tensor(tx_col[1], requires_grad = True).float().to(self.device);
+        
+        self.t_exp = torch.tensor(tx_exp[0], requires_grad = True).float().to(self.device); 
+        self.x_exp = torch.tensor(tx_exp[1], requires_grad = True).float().to(self.device);
+        
+        uv_init = tuple(torch.tensor(arr) for arr in uv_init)
+        uv_exp = tuple(torch.tensor(arr) for arr in uv_exp)
+        self.uv_init = torch.cat(uv_init, axis = 1).float().to(self.device)
+        self.uv_exp = torch.cat(uv_exp, axis = 1).float().to(self.device)
+        
+        #Parameters
+            #loss weight parameters
+        self.lambda_r = lambda_r
+        self.lambda_init = lambda_init
+        self.lambda_bound = lambda_bound
+            #causal decay
+        self.causal_decay = causal_decay
+            #curriculum
+        self.curriculum = curriculum
+            #ff_encod
+        self.encode_dim = encode_dim
+            #progressive 
+        self.progressive = progressive
+        for k,v in parameters.items():
+            setattr(self, k, v)
+        
+        self.optimizer = pinnUtils.choose_optimizer(optimizer, self.dnn.parameters())
+        self.iter = 0 
+        self.curr_on = None
+        self.bar = None
+        
+        if self.causal_decay is not None:
+            self.X_occurs = torch.stack([x == self.x_col.unique() for x in self.x_col]).float().to(self.device).T
+            self.M = torch.triu(torch.ones((self.X_occurs.shape[0], self.X_occurs.shape[0])), diagonal=1).to(self.device).T
+            self.epsilon_weight = epsilon_weight
+        
+        #log
+        self.thisLog = pinnUtils.DictLogger()
+        self.metrics = {'loss': 0,
+                       'mse': float('inf')}
+
+    def predict(self, t,x):
+        return self.dnn((t,x)) 
+    
+    def compute_grads(self, output, inputs, order = 1):
+        grads = output
+        for _ in range(order):
+            grads = torch.autograd.grad(
+                grads, inputs, 
+                grad_outputs = torch.ones_like(grads),
+                create_graph = True,
+                retain_graph = True
+            )[0]
+        return grads 
+    
+    def residual_loss(self, t,x): 
+        uv = self.predict(t,x)
+        
+        u = uv[:, 0:1]
+        v = uv[:, 1:2]
+        
+        ut = self.compute_grads(u,t)
+        vt = self.compute_grads(v,t) 
+        ut2 = self.compute_grads(ut, t)
+        vt2 = self.compute_grads(vt, t) 
+        ut3 = self.compute_grads(ut2,t)
+        vt3 = self.compute_grads(vt2,t) 
+        ux = self.compute_grads(u,x) 
+        vx = self.compute_grads(v,x)
+        
+        scalar = (torch.square(u) + torch.square(v))
+        residue_u = (ux + self.alpha*u/2
+                     -self.beta2/(2)*vt2
+                     -self.beta3/(6)*ut3
+                     +self.gamma*scalar*v
+                    )
+        
+        residue_v = (vx + self.alpha*v/2
+                 +self.beta2/(2)*ut2
+                 -self.beta3/(6)*vt3
+                 -self.gamma*scalar*u
+                )
+
+        residue = torch.cat([residue_u, residue_v], dim = 1)
+        return residue
+    
+    def boundary_loss(self,tL,xL, tU, xU):
+        uvL = self.predict(tL, xL)
+        uvU = self.predict(tU, xU)
+        uvLt = self.compute_grads(uvL,tL) 
+        uvUt = self.compute_grads(uvU, tU) 
+        
+        drichletUV = F.mse_loss(uvL, uvU)
+        neumannUV = F.mse_loss(uvLt, uvUt)
+        return drichletUV + neumannUV
+
+    def causal_loss(self,t,x):
+        uv_r = self.residual_loss(self.t_col, self.x_col)
+        L_t = (self.X_occurs @ uv_r.pow(2))/self.X_occurs.sum(1).reshape(-1,1)
+        W = torch.exp(-self.epsilon_weight*(self.M @ L_t)).detach()
+        return W, L_t
+
+    def loss_func(self):
+        if torch.is_grad_enabled():
+            self.optimizer.zero_grad()
+        uv_ip = self.predict(self.t_init, self.x_init) 
+        loss_b = self.boundary_loss(self.tL_bound, self.xL_bound, 
+                                    self.tU_bound, self.xU_bound)
+
+        if not self.causal_decay:
+            uv_r = self.residual_loss(self.t_col, self.x_col)
+            residue = torch.sum(torch.square(uv_r), dim =1).mean()
+        else:
+            W, L_t = self.causal_loss(self.t_col, self.x_col)
+            self.thisLog.add({'causal_minweight': W.min().item()})
+            residue = (W*L_t).mean()
+
+        loss_init = F.mse_loss(uv_ip, self.uv_init)
+        loss = self.lambda_bound*loss_b + self.lambda_init*loss_init + self.lambda_r*residue
+
+        self.pbar.set_postfix(self.metrics)
+
+        self.thisLog.add({
+        "boundary_loss": loss_b.item(),
+        "residual_loss": residue.item(),
+        "init_loss": loss_init.item(),
+        "total_loss": loss.item()
+        })
+        
+        if loss.requires_grad:
+            loss.backward()
+        
+        self.metrics['loss'] = loss.item()
+        self.iter += 1
+        self.thisLog.add({'epoch': self.iter})
+
+        return loss
+
+    def train(self, epochs = 3000, curriculum: dict = None, progressive: dict = None):
+        self.epochs = epochs 
+        self.dnn.train()
+
+        if self.curriculum is not None:
+            self.curriculum_learning(self.curriculum)
+
+            for curr_val in self.curr_range:
+                setattr(self, self.curr_on, curr_val)
+
+                if curr_val == self.curr_range[-1] and curriculum['version'] ==2:
+                    self.curr_epochs = self.epochs-self.iter
+                self.thisLog.add({f'self.curr_on': curr_val})
+                self.pbar = trange(self.curr_epochs, desc = f'[CURR] Training on {self.curr_on}: {curr_val:.3}')
+                for _ in self.pbar:
+                    self.dnn.train() 
+                    self.optimizer.step(self.loss_func)
+                    if  self.iter % min(self.epochs//10, 100) == 0:
+                        self.validate(self.t_exp, self.x_exp, self.uv_exp)
+        
+        elif self.progressive is not None:
+            self.progressive_learning(self.progressive)
+            self.causal_decay = False #this version don't support causal_decay
+            X_COL = self.x_col.detach().clone()
+            T_COL = self.t_col.detach().clone()
+            maxL = torch.max(X_COL).cpu().numpy(); minL = torch.min(X_COL).cpu().numpy()
+            maxT = torch.max(T_COL).cpu().numpy(); minT = torch.min(T_COL).cpu().numpy()
+            N = X_COL.shape[0]
+
+            for index in self.prog_range:
+                num_sampling = int(index*N/self.prog_range[-1])
+                self.progressive_resampling(np.array([minT, minL]), np.array([maxT, index*maxL/self.prog_range[-1]]), num_sampling)
+                assert self.prog_sampling <= self.x_col.shape[0], (
+                    f"prog_sampling={self.prog_sampling} exceeds available collocation points ({self.x_col.shape[0]}) "
+                )
+
+                if index == self.prog_range[-1] and progressive['version'] ==2:
+                    self.prog_epochs = self.epochs-self.iter
+                self.thisLog.add({f'self.prog_on': index})
+                self.pbar = trange(self.prog_epochs, desc = f'[PROG] Training on Index: {index}')
+                for _ in self.pbar:
+                    self.dnn.train() 
+                    self.optimizer.step(self.loss_func)
+                    if  self.iter % min(self.epochs//10, 100) == 0:
+                        self.validate(self.t_exp, self.x_exp, self.uv_exp)
+                
+                if not index == self.prog_range[-1]:
+                    rand_idx = torch.randint(0, self.x_col.shape[0], (self.prog_sampling,), device=self.x_col.device)
+                    x_known = self.x_col[rand_idx]
+                    t_known = self.t_col[rand_idx]
+                    with torch.no_grad():
+                        uv_known = self.predict(t_known, x_known)
+
+                    #Concatenate dataset
+                    self.x_init = torch.cat([self.x_init.detach(), x_known.detach()], dim=0).requires_grad_()
+                    self.t_init = torch.cat([self.t_init.detach(), t_known.detach()], dim=0).requires_grad_()
+                    self.uv_init = torch.cat([self.uv_init.detach(), uv_known.detach()], dim=0).requires_grad_()
+            
+        else:
+            self.pbar = trange(epochs, desc = f'[{self.net}{"/causal" if self.causal_decay else ""}{"/encPBC" if self.encode_dim else ""}] training')
+            for _ in self.pbar:
+                self.dnn.train()
+                self.optimizer.step(self.loss_func)
+
+                if self.iter % 100 == 0:
+                    mse = self.validate(self.t_exp, self.x_exp, self.uv_exp)
+        return 
+    
+    def curriculum_learning(self, curriculum):
+        print(f"Using curriculum learning on param {curriculum['curr_on']} with target {getattr(self, curriculum['curr_on'])}\n")
+        self.curr_on = curriculum['curr_on']
+        self.curr_range = np.linspace(*sorted([curriculum['curr_initval'], getattr(self, self.curr_on)]),
+                                      curriculum['curr_steps'])
+        if curriculum['curr_initval'] > getattr(self, self.curr_on):
+            self.curr_range = self.curr_range[::-1] # the bigger the easier
+        setattr(self, self.curr_on, self.curr_range[0])
+        
+        if curriculum['version'] == 1:
+            self.curr_epochs = self.epochs // curriculum['curr_steps']
+        elif curriculum['version'] == 2:
+            assert curriculum['curr_epochs'] * curriculum['curr_steps'] < self.epochs, "More epochs are needed for Curriculum v2"
+            self.curr_epochs = curriculum['curr_epochs']
+        else:
+            raise ValueError(f"Wrong version selection (selected {curriculum['version']} instead of 1 or 2)")
+    
+    def progressive_learning(self, progressive):
+        N = progressive['domain_stages']
+        print(f"Progressive PT learning is used for {N} steps")
+        self.prog_range = np.arange(1, N+1)
+        self.prog_sampling = progressive['sampling']
+        
+        if progressive['version'] == 1:
+            self.prog_epochs = self.epochs//N
+        elif progressive['version'] == 2:
+            assert progressive['prog_epochs']*progressive['domain_stages'] < self.epochs, "More epochs are needed for Progressive PT v2"
+            self.prog_epochs = progressive['prog_epochs']
+        else:
+            raise ValueError(f"Wrong version selection (selected {progressive['version']} instead of 1 or 2)")
+                                                            
+                                                            
+    def progressive_resampling(self, start, final, n):
+        tx_col = pinnUtils.generate_points(start, final, n, seed = None)
+        t_col = tx_col[:,0].reshape(-1,1)
+        x_col = tx_col[:,1].reshape(-1,1)
+        self.t_col = torch.tensor(t_col, requires_grad = True).float().to(self.device); 
+        self.x_col = torch.tensor(x_col, requires_grad = True).float().to(self.device);
+                                                            
+        
+    def validate(self, t,x, uv):
+        self.dnn.eval()
+        uv_p = self.predict(t,x)
+        uv_p = uv_p.detach()
+        mse = torch.square(uv_p - uv).mean()
+        self.thisLog.add({"valid_mse": mse.cpu().numpy()})
+        self.metrics['mse'] = mse.item()
+        return mse
+    
+    def inference(self, tMesh, xMesh):
+        T = tMesh.reshape(-1,1); X = xMesh.reshape(-1,1) 
+        T = torch.tensor(T).float().to(self.device); X = torch.tensor(X).float().to(self.device)
+        self.dnn.eval()
+        with torch.no_grad():
+            uv = self.predict(T,X)
+        
+        pulse = torch.complex(uv[:, 0], uv[:, 1]).cpu().numpy()
+        pulse = pulse.reshape(tMesh.shape)
+        return pulse
+    
+    def save_model(self, model_path, json_path):
+        torch.save(self.dnn.state_dict(), model_path)
+        self.thisLog.save(json_path)
+    
+                                                            
+
+
+
+            
---
+++ pinnPack/pinnUtils.py
@@ -0,0 +1,129 @@
+import torch 
+import torch.nn as nn
+import numpy as np 
+import json
+from ssfmPack import utils
+
+#Activation Function
+    
+def find_activation(activation):
+    act_fn = {
+    'tanh': nn.Tanh,
+    'relu': nn.ReLU,
+    'sigmoid': nn.Sigmoid,
+    'swish': nn.SiLU,
+    }.get(activation.lower(), nn.Tanh)
+    
+    return act_fn 
+
+#Optimizers 
+def choose_optimizer(optimizer_name: str, *args, **kwargs):
+    if optimizer_name.lower() == 'lbfgs':
+        return LBFGS(*args, **kwargs)
+    elif optimizer_name.lower() == 'adam':
+        return Adam(*args, **kwargs)
+
+def LBFGS(model_param,
+        lr=1.0,
+        max_iter=10000,
+        max_eval=None,
+        history_size=50,
+        tolerance_grad=1e-20,
+        tolerance_change=1e-20,
+        line_search_fn="strong_wolfe"):
+
+    optimizer = torch.optim.LBFGS(
+        model_param,
+        lr=lr,
+        max_iter=max_iter,
+        max_eval=max_eval,
+        history_size=history_size,
+        tolerance_grad=tolerance_grad,
+        tolerance_change=tolerance_change,
+        line_search_fn=line_search_fn
+        )
+
+    return optimizer
+
+def Adam(model_param, lr=1e-4, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False):
+
+    optimizer = torch.optim.Adam(
+                model_param,
+                lr=lr,
+    )
+    return optimizer
+
+#Logging Information on Dict.
+class DictLogger:
+    def __init__(self):
+        self.history = {}
+    
+    def add(self, log_dict):
+        for key, value in log_dict.items():
+            if key not in self.history:
+                self.history[key] = []
+            self.history[key].append(value)
+    
+    def save(self, path):
+        history  = {k: [float(v) for v in vs] for k, vs in self.history.items()}
+        with open(path, 'w') as f:
+            json.dump(history, f, indent = 4)
+
+#Sampling 
+def sobol_sampling(n: int, d: int, seed: int = 0) -> np.ndarray:
+    """
+    Sobol Sampling using torch.quasirandom
+
+    Args:
+        n (int): Number of samples
+        d (int): Dimensions
+        seed (int, optional): Seed for reproducibility
+
+    Returns:
+        np.ndarray: Sobol samples scaled to [0, 1]^d
+    """
+    scramble = True if seed is None else False
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    sobol = torch.quasirandom.SobolEngine(dimension=d, scramble = scramble)
+    samples = sobol.draw(n)  
+    return samples.numpy()
+
+
+def generate_points(start: np.ndarray, final: np.ndarray, n: int, seed: int = 0) -> np.ndarray:
+    '''
+    Initialize points using Latin Hypercube Sampling
+
+    Args:
+        final (np.ndarray): Expected final point (have to have the same dimension with initial)
+        initial (np.ndarray): Expected initial point (have to have the same dimension with final)
+        n (int): Number of samples
+        seed (int, optional): Random seed. Defaults to None.
+    '''
+    d = final.shape[-1] if isinstance(final, np.ndarray) else 1
+    points = sobol_sampling(n,d,seed)*(final-start) + start
+    return points
+
+def ssfm_sampling(timeArray, lengthArray, pulse, num_sample = 2000, clipRange = 500, normalized = True):
+    clipTime = utils.clipMatrix(timeArray, clipRange)
+    pulse = utils.clipMatrix(pulse, clipRange)
+    T,L = np.meshgrid(clipTime, lengthArray) 
+
+    # Sample Sobol points in 2D
+    samples =generate_points(np.array([0,0]), np.array([1,1]), num_sample)*pulse.shape
+    samples = samples.astype(int)
+
+    Ts = T[samples[:,0], samples[:,1]].reshape(-1,1)
+    Ls = L[samples[:,0], samples[:,1]].reshape(-1,1)
+    ps = pulse[samples[:,0], samples[:,1]].reshape(-1,1)
+    if normalized:
+        Ts = Ts/max(timeArray)
+        Ls = Ls/max(lengthArray)
+    
+    us = np.real(ps)
+    vs = np.imag(ps)
+
+    tx = (Ts, Ls)
+    uv = (us, vs)
+    return tx, uv
